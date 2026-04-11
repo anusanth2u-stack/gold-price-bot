c179ff0e829f429184c4c080ba83fe9ee7d393f0
anusanth2u-stack <anusanth2u@gmail.com>
2026-04-11 17:55:44 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
 
 URL = "https://www.keralagold.com/kerala-gold-rate-per-gram.htm"
 
-def get_gold_price():
+def get_price():
     res = requests.get(URL)
     soup = BeautifulSoup(res.text, "html.parser")
     text = soup.get_text()
@@ -15,13 +16,49 @@ def get_gold_price():
     return None
 
 
-def calculate_score(price, last_price=None):
+def calculate_score(price, history):
     score = 50
 
-    if last_price:
-        if price < last_price:
+    if history:
+        if price < history[-1]:
             score += 20
         else:
             score -= 10
 
+    if len(history) >= 3:
+        if price < history[-1] < history[-2]:
+            score += 20
+
+    avg = sum(history)/len(history) if history else price
+
+    if price < avg:
+        score += 10
+    else:
+        score -= 5
+
     return max(0, min(score, 100))
+
+
+def long_term_decision(score, bought):
+    day = datetime.now().day
+
+    if bought:
+        return "✅ DONE"
+
+    if score >= 70:
+        return "🟢 BUY"
+
+    if day >= 20:
+        return "⚠️ FORCE BUY"
+
+    return "⏳ WAIT"
+
+
+def short_term_decision(score, cash):
+    if score >= 75:
+        return min(2000, cash), "BUY"
+
+    if score <= 30:
+        return 1000, "SELL"
+
+    return 0, "HOLD"
